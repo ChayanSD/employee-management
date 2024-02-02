79c74fdd55b67b9ab2ef0dee1bd0dbea2026f893
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2024-02-02 23:34:01 +0600

    Backend completed with all endpoints

---
+++ src/app/api/employee/[employeeId]/route.js
@@ -0,0 +1,110 @@
+import { Employee } from "@/models/Employee";
+import { NextResponse } from "next/server";
+
+export async function GET(request , {params}){
+    const {employeeId} = params;
+    try {
+        
+        const getSingleEmployee = await Employee.findById(employeeId)
+
+       
+        return NextResponse.json(
+            {
+                getSingleEmployee,
+            },
+            {
+                status: 200,
+            }
+        )
+    } catch (error) {
+        console.log(error);
+
+        
+        return NextResponse.json(
+            {
+                error: 'failed to get single employee',
+            },
+            {
+                status: 404,
+            }
+        )
+    }
+}
+
+export async function PUT(request , {params}){
+    const {employeeId} = params;
+
+    const {name, email, address, salary} =await request.json();
+
+    try {
+        
+        let employee = await Employee.findById(employeeId);
+       
+        employee.name = name;
+        employee.email = email;
+        employee.address = address;
+        employee.salary = salary;
+
+        //saved in db 
+        const updatedEmployee = await employee.save();
+
+        return NextResponse.json(
+            {
+                updatedEmployee,
+                message: "Employee Updated Successfully"
+            },
+            {
+                status: 201
+            }
+        )
+
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            {
+                error: 'failed to update employee',
+            },
+            {
+                status: 404,
+            }
+        )
+
+        
+    }
+}
+
+//Delete employee by id
+
+export async function DELETE(request, { params }) {
+    // Get employeeId From params 
+   const { employeeId } = params;
+
+   try {
+       await Employee.deleteOne({
+           _id: employeeId
+       })
+
+       // Return message And Status 
+       return NextResponse.json(
+           {
+               message: "Employee deleted successfully"
+           },
+           {
+               status: 201
+           }
+       )
+   } catch (error) {
+       console.log(error)
+
+       // Return Error And Status 
+       return NextResponse.json(
+           {
+               error: 'failed to delete employee',
+           },
+           {
+               status: 404,
+           }
+       )
+   }
+}
---
+++ src/app/api/employee/route.js
@@ -0,0 +1,99 @@
+// import connectDB from '../../../database/db.js'
+
+import { connectDB } from "@/database/db";
+import { Employee } from "@/models/Employee";
+import { NextResponse } from "next/server";
+
+
+connectDB();
+
+
+export async function GET(request){
+    try {
+
+        const getEmployee = await Employee.find();
+
+        return NextResponse.json(getEmployee);
+        
+    } catch (error) {
+        console.log(error);
+
+        return NextResponse.json({
+            error : 'Failed to get employee'
+        },
+        {
+            status : 404
+        })
+    }
+};
+
+
+
+export async function POST(request){
+   // get Employee data from frontend 
+   const { name, email, address, salary } = await request.json();
+
+   // validation 
+   if (!name || !email || !address || !salary) {
+       return NextResponse.json(
+           {
+               error: "All fields must be required",
+           },
+           {
+               status: 404,
+           }
+       )
+   }
+
+   // find employee through email
+   const empl = await Employee.findOne({ email })
+
+   // Condition 
+   if (empl) {
+       return NextResponse.json(
+           {
+               error: "This employee already exists"
+           },
+           {
+               status: 404,
+           }
+       )
+   }
+
+   // Create Employee
+   const employee = new Employee({
+       name,
+       email,
+       address,
+       salary
+   })
+
+   try {
+       // Create Saved Employee 
+       const savedEmployee = await employee.save();
+
+       // Return savedEmployee, message, and status
+       return NextResponse.json(
+           {
+               savedEmployee,
+               message: "Employee saved successfully"
+           },
+           {
+               status: 200,
+           }
+       )
+   } catch (error) {
+       console.log(error);
+
+       // return error and status 
+       return NextResponse.json(
+           {
+               error: "Failed to save employee",
+           },
+           {
+               status: 404,
+           }
+       )
+   }
+
+}
